42eb7fd7f49f474bdd08b89f4d3ec9487d291dc7
Naveen1854 <kusuma.naveen1854@gmail.com>
2022-09-30 16:00:37 +0530

    new file added

---
+++ Alphabets.java
@@ -0,0 +1,7 @@
+public class Alphabets {
+        public static void main (String args[]){
+        char  ch;
+        for (ch='A';ch<='Z';ch++)
+        System.out.println(ch+"  \n ");
+        }
+    }

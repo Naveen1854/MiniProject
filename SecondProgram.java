161d852c6a73790cc3132b31efb6832db9608142
Naveen1854 <kusuma.naveen1854@gmail.com>
2022-10-02 17:34:44 +0530

    File Added

---
+++ SecondProgram.java
@@ -0,0 +1,8 @@
+public class SecondProgram {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 20;
+        System.out.println("The addition of " + a + " and " + b + " is: " + (a + b));
+    }
+}
